3043f0a6a418a8a8c94d27e0179bc95e41a952bf
Dimipilo92 <pilodi12@gmail.com>
2018-05-26 13:46:49 -0700

    Make board display easier to follow

--- src/cs431/p3/BoardDisplay.java
+++ src/cs431/p3/BoardDisplay.java
@@ -1,29 +1,47 @@
 package cs431.p3;
 
 public class BoardDisplay {
+	
 	public static String display(Board b, Player[] p) {
 		StringBuilder sb = new StringBuilder();
 		String[] historyList = displayableHistoryList(b);
 		sb.append("  ");
+		
+		// above the header (history > 9)
+		int h = 0;
+		if (historyList.length > b.getBoard().length) {
+			sb.append("\t\t\t" + p[0].getName() + " vs. " + p[1].getName() +"\n");
+			for (; h <= historyList.length-b.getBoard().length-2; h++) {
+				sb.append("\t\t\t"+(h+1)+". "+historyList[h]+"\n");
+			}
+		}
+		
+		// header
 		for (int i = 1; i <= b.getBoard().length; i++) {
 			sb.append(i+" ");
 		}
-		sb.append("\t" + p[0].getName() + " vs. " + p[1].getName() +"\n");
+		if (historyList.length <= b.getBoard().length) {
+			sb.append("\t" + p[0].getName() + " vs. " + p[1].getName() +"\n");
+		}
+		else {
+			sb.append("\t"+(h+1)+". "+historyList[h]+"\n");
+			h++;
+		}
+		
+		// below the header
 		for (int i = 0; i < b.getBoard().length; i++) {
 			sb.append( (char)('A'+i) + " " );
 			for (int j = 0; j < b.getBoard().length; j++) {
 				sb.append(b.getBoard()[i][j] + " ");
 			}
-			if (i < historyList.length) {
-				sb.append("\t"+(i+1)+". "+historyList[i]);
+			if (h < historyList.length) {
+				sb.append("\t"+(h+1)+". "+historyList[h]);
+				h++;
 			}
 			sb.append("\n");
 		}
-		if (historyList.length >= b.getBoard().length) {
-			for (int i = b.getBoard().length; i < historyList.length; i++) {
-				sb.append("\t\t\t"+(i+1)+". "+historyList[i]+"\n");
-			}
-		}
+		
+		// below the board
 		if (historyList.length>0) {
 			sb.append("\n"+p[(b.getHistory().length+1)%2] + "'s Move is: "
 					+ b.getLastMove() + "\n");
